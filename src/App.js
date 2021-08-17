7477b6d3a31fa36a714edb5b48f3956f191ba899
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-08-16 21:59:38 -0400

    create files with components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Navbar from './components/Navbar.js/Navbar';
+import ProductList from './components/ProductList/ProductList';
+import WineDetails from './components/WineDetails/WineDetails';
 import './App.css'
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
+export class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar/>
+        <ProductList/>
+        <WineDetails/>
+      <h1 className="text-white">HELLO FROM APP</h1>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
+
